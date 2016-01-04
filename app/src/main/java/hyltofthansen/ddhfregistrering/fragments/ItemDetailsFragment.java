aa886fe3f2fcf1cad47bb97d60940e1d6c4f50d8
Stefan Hyltoft <stefanhyltoft@gmail.com>
2016-01-04 13:44:12 +0100

    Item details POC

--- app/src/main/java/hyltofthansen/ddhfregistrering/fragments/ItemDetailsFragment.java
+++ app/src/main/java/hyltofthansen/ddhfregistrering/fragments/ItemDetailsFragment.java
@@ -7,10 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 import hyltofthansen.ddhfregistrering.R;
+import hyltofthansen.ddhfregistrering.dao.GetHTTPDetails;
 import hyltofthansen.ddhfregistrering.dto.ItemDTO;
 
 /**
@@ -19,21 +23,25 @@ import hyltofthansen.ddhfregistrering.dto.ItemDTO;
 public class ItemDetailsFragment extends Fragment {
 
     ItemDTO item;
+    View root;
+    ArrayList<ItemDTO> items;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.itemdetails, container, false);
-        getActivity().setTitle("Genstand detaljer"); // TODO: FRÆKT AT SÆTTE GENSTANDSNAVN HER
+        root = inflater.inflate(R.layout.itemdetails, container, false);
+        getActivity().setTitle(item.getItemheadline().toString()); // TODO: FRÆKT AT SÆTTE GENSTANDSNAVN HER
+        items = new ArrayList<ItemDTO>();
+        GetHTTPDetails getHTTPDetails = new GetHTTPDetails(getActivity(), item.getItemid(), items, this);
+        getHTTPDetails.fetchItems();
+        return root;
+    }
+
+    public void updateTextViews() {
         TextView tv_headline = (TextView) root.findViewById(R.id.tv_itemheadline);
-        tv_headline.setText(item.getItemheadline());
+        tv_headline.setText(items.get(0).getItemheadline().toString());
 
         TextView tv_descript = (TextView) root.findViewById(R.id.tv_itemdescr);
-        tv_descript.setText(item.getItemdescription());
-
-        TextView tv_received = (TextView) root.findViewById(R.id.tv_itemreceived);
-        tv_received.setText(item.getItemreceived());
-
-        return root;
+        tv_descript.setText(items.get(0).getItemdescription().toString());
     }
 
     public void setItem(ItemDTO item) {
